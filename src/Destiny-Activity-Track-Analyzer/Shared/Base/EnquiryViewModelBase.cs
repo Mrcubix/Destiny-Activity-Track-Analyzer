4d5756728df94a43859b91297b9882cee4f91a21
Mrcubix <tintamare91@gmail.com>
2022-10-27 15:38:47 +0200

    Added some Documentation to EnquiryViewModelBase

--- src/Destiny-Activity-Track-Analyzer/Shared/Base/EnquiryViewModelBase.cs
+++ src/Destiny-Activity-Track-Analyzer/Shared/Base/EnquiryViewModelBase.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Runtime.InteropServices;
-using System.Security.Authentication;
 using System.Threading.Tasks;
 using API.Endpoints;
-using API.Entities.User;
 using ReactiveUI;
 using Tracker.Shared.Frontend;
-using Tracker.Shared.Static;
 using Tracker.Shared.Stores;
 using Tracker.Shared.Stores.Component;
 
@@ -29,30 +21,45 @@ namespace Tracker.ViewModels
         protected string _errorColor = "";
 
 
+        /// <summary>
+        ///     An Shortcut toward an existing instance of <see cref="SettingsStore"/>
+        /// </summary>
         public virtual SettingsStore SettingsStore
         {
             get => _settingsStore;
             set => this.RaiseAndSetIfChanged(ref _settingsStore, value);
         }
 
+        /// <summary>
+        ///     An Shortcut toward an existing instance of <see cref="DefaultsStore"/>
+        /// </summary>
         public virtual DefaultsStore DefaultsStore
         {
             get => _defaultsStore;
             set => this.RaiseAndSetIfChanged(ref _defaultsStore, value);
         }
 
+        /// <summary>
+        ///     An Shortcut toward an existing instance of <see cref="UserStore"/>
+        /// </summary>
         public virtual UserStore UserStore
         {
             get => _userStore;
             set => this.RaiseAndSetIfChanged(ref _userStore, value);
         }
 
+        /// <summary>
+        ///     The current error message or status
+        /// </summary>
         public virtual string CurrentError
         {
             get => _currentError;
             set => this.RaiseAndSetIfChanged(ref _currentError, value);
         }
 
+        /// <summary>
+        ///     The color of the current error message or status
+        /// </summary>
         public virtual string ErrorColor
         {
             get => _errorColor;
@@ -69,19 +76,43 @@ namespace Tracker.ViewModels
             Initialize();
         }
 
+        /// <summary>
+        ///     A method called on initialization <br />
+        ///     Subscribe to the <see cref="Tracker.Shared.Stores.SettingsStore.SettingsUpdated" /> event
+        /// </summary>
         public virtual void Initialize()
         {
-            Remote.SharedStores.SettingsStore.SettingsUpdated += OnSettingsChange;
+            SettingsStore.SettingsUpdated += OnSettingsChange;
         }
 
+        /// <summary>
+        ///     Check whether or not the view overriding it should be shown
+        /// </summary>
+        /// <param name="settings">The current settings</param>
+        /// <returns>Whether or not the view should be shown</returns>
         public abstract bool ShouldEnquire(AppSettings settings);
 
+        /// <summary>
+        ///     Show the overriding view
+        /// </summary>
         public abstract void Enquire();
 
+        /// <summary>
+        ///     Used to run neccessary checks after saving
+        /// </summary>
         public abstract Task Save();
 
+        /// <summary>
+        ///     Used to skip parts of the setup
+        /// </summary>
         public abstract void Skip();
 
+        /// <summary>
+        ///     A method called when the settings are updated <br />
+        ///     Used to update the Endpoint instance when settings eventually get loaded or modified
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="settings">An instance of <see cref="Tracker.Shared.Stores.Component.AppSettings" /></param>
         public virtual void OnSettingsChange(object? send, AppSettings settings)
         {
             this.API = new(settings.APISettings);
